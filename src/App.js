b01a952137ee56aeba6907630cf46b8e36cbbd78
Hanief00 <hanifadaadaaja@gmail.com>
2025-03-03 00:51:08 +0700

    Done Praktikum 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Counter from './Counter';
+import Greeting from './Greeting';
+import TodoList from './TodoList';
+
+function Example() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [email, setEmail] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder="Nama" value={name} onChange={handleNameChange} />
+      <input type="number" placeholder="Umur" value={age} onChange={handleAgeChange} />
+      <input type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
+      <p>
+        {name} berumur {age} tahun dan emailnya adalah {email}.
+      </p>
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Aplikasi React Saya</h1>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <h2>Selamat datang di aplikasi React pertama saya!</h2>
+      <p>Ini adalah area konten utama</p>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2025 React Saya</p>
+    </footer>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +77,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <Main />
+      <Greeting name="Soto" />
+      <Counter />
+      <Example />
+      <TodoList />
+      <Footer />
     </div>
   );
 }
